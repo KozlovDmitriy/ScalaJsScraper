1db67911b1643710e2ec31a7ea65ae16009a7a00
Dmitriy Kozlov <kozlov.dmitriy.victorovich@gmail.com>
2016-05-16 12:02:23 +0300

    [+] downloaded files from console

--- app/console/main.scala
+++ app/console/main.scala
@@ -1,54 +1,54 @@
 package console
 
 import java.io.File
+import java.net.URL
 
 import Scraper.JsScraper
-import play.api.libs.json.Json
 
 import scala.io.Source
-import scala.util.{Failure, Success}
-
-case class Config( pages: File = new File("."), scrap: File = new File(".") )
+import scala.sys.process._
+import scala.util.{Failure, Success, Try}
 
 /**
   * Точка входа консольного приложения
   */
 object MainConsole {
 
-  /*val parser = new scopt.OptionParser[Config]("scopt") {
-    head("scopt", "3.x")
-    opt[File]('p', "pages") required() valueName ("<file>") action { (x, c) =>
-      c.copy(pages = x)
-    } text ("getPages script file")
-    opt[File]('s', "scrap") required() valueName ("<file>") action { (x, c) =>
-      c.copy(scrap = x)
-    } text ("scrap script file")
-  }*/
+  def downloadFile(url: String, dir: String) = Try {
+    val fileUrl = new URL(url)
+    val fileName = url.split("/").last
+    val file: File = new File(dir, fileName)
+    fileUrl #> file !!
+  }
 
-  def scrap(jsPages: String, jsScrap: String) = JsScraper.jsScrap(jsPages, jsScrap) match {
-    case Success(s) => println( Json.toJson(s) )
+  def scrap(dir: String, jsPages: String, jsScrap: String) = JsScraper.jsScrap(jsPages, jsScrap) match {
+    case Success(s) => {
+        s.foreach( url => downloadFile(url, dir) match {
+          case Failure(f) => println( url + " failed" )
+          case Success(_) => println( url + " correctly" )
+        })
+    }
     case Failure(f) => println( f getMessage )
   }
 
   def main(args: Array[String]) {
-    /*parser.parse(args, Config()) match {
-      case Some(config) =>
-        println( config )
-      // do stuff
-
-      case None =>
-      // arguments are bad, error message will have been displayed
-    }*/
-    val sources = args.map( path => {
-      val source = Source.fromFile( path )
-      val content = try source.mkString catch {
-        case e => {
-          println(e.getMessage())
-          ""
-        }
-      } finally source.close()
-      content
-    } ).reduce(_+_)
-    scrap(sources, sources)
+    if (args.length > 1) {
+      val dir = args(0)
+      if (new File(dir).mkdir()) {
+        val sources = args.drop(1).map( path => {
+          val source = Source.fromFile( path )
+          val content = try source.mkString catch {
+            case e: Exception => {
+              println( e.getMessage() )
+              ""
+            }
+          } finally source.close()
+          content
+        } ).reduce(_+_)
+        scrap(dir, sources, sources)
+      } else {
+        println( "Warning: cant create downloaded dir" )
+      }
+    }
   }
 }
